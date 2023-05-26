f866231521a7e175c40100cf6a5b98082b7a2e94
vasanthr7 <vasanthrengaraj10@gmail.com>
2023-05-26 15:07:55 +0530

    my change
    my change

--- src/org/git/Hi.java
+++ src/org/git/Hi.java
@@ -3,7 +3,7 @@ package org.git;
 public class Hi {
 	
 	public static void add() {
-		System.out.println("add");
+		System.out.println("adddfbbhrtnhtrntr");
 	}
 	
 	public static void main(String[] args) {
